2a715753df431428430202013098b10e92fc8d92
G016 <g.pverma716@gmail.com>
2022-06-23 10:55:57 +0530

    My Cal.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'KCC Institude Of Technology And Managament' });
+  res.render('index', { title: 'KCC Institute Of Technology And Management' });
   // console.log(req)
   // res.json({name:"Gaurav Verma"})
 });
 
+router.all('/add', function (req, res, next) {
+  console.log("Adding two number");
+  console.log(req.body);
+  var num1 = parseFloat(req.body.a);
+  var num2 = parseFloat(req.body.b);
+  console.log(num1);
+  console.log(num2);
+  var result = num1 + num2;
+  console.log(result);
+  res.json(result);
+})
+
+router.all('/sub', function (req, res, next) {
+  console.log(req.body);
+  var num1 = parseFloat(req.body.a);
+  var num2 = parseFloat(req.body.b);
+  var result = num1 - num2;
+  console.log(result);
+  res.json(result);
+})
+
+router.all('/mul', function (req, res, next) {
+  console.log(req.body);
+  var num1 = parseFloat(req.body.a);
+  var num2 = parseFloat(req.body.b);
+  var result = num1 * num2;
+  console.log(result);
+  res.json(result);
+})
+
 router.post('/signup', (req, res) => {
   var em = req.body.emailInput;
-  res.render('index', { email: em, name: req.body.nameInput, title: "Product Based Company" });
+  res.render('index', { email: em, name: req.body.nameInput, title: "Coding Ninja" });
 });
 
 module.exports = router;
