fc032a52e4584fb837ae593b76ab99c2db6e5726
chandan tiwari <chandant157btechcse2024@kccitm.edu.in>
2022-06-30 13:42:32 +0530

    cal.

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,79 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createPool({
+  connectionLimit: 1000,
+  host: 'easylearning.guru',
+  user: 'kcc_student',
+  password: 'Kccitm.edu.in1',
+  database: 'kccStudent'
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'KCC Institute Of Technology And Management' });
-  // console.log(req)
-  // res.json({name:"Gaurav Verma"})
+  // res.render('index', { title: 'Express' });
+  con.getConnection(function (err, connection) {
+    connection.query("SELECT * FROM  ritik_ct ORDER BY id", function (err, results) {
+      if (err) throw err;
+      else console.log(results);
+      res.render('index', { "data": results, title: 'express' });
+    });
+  });
 });
 
-router.all('/add', function (req, res, next) {
-  console.log("Adding two number");
-  console.log(req.body);
-  var num1 = parseFloat(req.body.a);
-  var num2 = parseFloat(req.body.b);
-  console.log(num1);
-  console.log(num2);
-  var result = num1 + num2;
-  console.log(result);
-  res.json(result);
+router.post('/add', function (req, res, next) {
+  var x = parseInt(req.body.a);
+  var y = parseInt(req.body.b);
+  var z = parseInt(req.body.c);
+  var op = "+";
+  var total = x + y + z;
+  res.json(total)
+  console.log(total);
+  con.getConnection(function (err, connection) {
+    connection.query("INSERT INTO ritik_ct (num1, num2 , num3, op, res) VALUES ('" + x + "', '" + y + "', '" + z + "', '" + op + "','" + total + "')", function (err, rows) {
+      connection.release();
+      if (err) throw err;
+      else console.log(rows.length);
+    });
+  });
 })
 
-router.all('/sub', function (req, res, next) {
-  console.log(req.body);
-  var num1 = parseFloat(req.body.a);
-  var num2 = parseFloat(req.body.b);
-  var result = num1 - num2;
-  console.log(result);
-  res.json(result);
+router.post('/mul', function (req, res, next) {
+  var x = parseInt(req.body.a);
+  var y = parseInt(req.body.b);
+  var z = parseInt(req.body.c);
+  var op = "*";
+  var total = x * y * z;
+  res.json(total)
+  console.log(total);
+  con.getConnection(function (err, connection) {
+    connection.query("INSERT INTO ritik_ct (num1, num2 , num3, op, res) VALUES ('" + x + "', '" + y + "', '" + z + "', '" + op + "','" + total + "')", function (err, rows) {
+      connection.release();
+      if (err) throw err;
+      else console.log(rows.length);
+    });
+  });
 })
 
-router.all('/mul', function (req, res, next) {
-  console.log(req.body);
-  var num1 = parseFloat(req.body.a);
-  var num2 = parseFloat(req.body.b);
-  var result = num1 * num2;
-  console.log(result);
-  res.json(result);
-})
-router.all('/div', function (req, res, next) {
-  console.log(req.body);
-  var num1 = parseFloat(req.body.a);
-  var num2 = parseFloat(req.body.b);
-  var result = num1 / num2;
-  console.log(result);
-  res.json(result);
-})
+router.post('/delete', function (req, res, next) {
+  console.log(req.body.id)
+  con.query("DELETE FROM ritik_ct where id =" + req.body.id, function (err, result) {
+    if (err) res.json({ code: 0 });
+    console.log(result);
+    res.json({ code: 1 })
+  });
+});
 
-router.post('/signup', (req, res) => {
-  var em = req.body.emailInput;
-  res.render('index', { email: em, name: req.body.nameInput, title: "Coding Ninja" });
+router.post('/totalsum', function (req, res, next) {
+  console.log(req.body)
+  con.getConnection(function (err, connection) {
+    connection.query("SELECT * FROM  ritik_ct", function (err, results) {
+      if (err) throw err;
+      else console.log(results);
+      res.json(results)
+    });
+  });
 });
 
 module.exports = router;
